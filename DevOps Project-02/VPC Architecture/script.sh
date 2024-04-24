b318d924f39818dcaeacb766df114e28b257d5eb
chennareddy5 <63826431+chennareddy5@users.noreply.github.com>
2024-04-24 14:39:37 +0530

    Update script.sh

--- DevOps Project-02/VPC Architecture/script.sh
+++ DevOps Project-02/VPC Architecture/script.sh
@@ -2,5 +2,5 @@
 yum update -y
 aws s3 cp s3://ed-web-config-project-s3/index.html  /var/www/html
 systemctl restart httpd
-sudo yum install awslogs -y
+sudo yum install awslogsd -y
 sudo systemctl start awslogsd
